f0de32e909ebd95116bfe41dac1ec489f06d4e2e
seonghunYang <didtjdgns852@naver.com>
2020-06-11 04:48:05 +0900

    NewScreen 구현

--- Screen/NewsScreen.js
+++ Screen/NewsScreen.js
@@ -1,8 +1,59 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import styled from 'styled-components/native'
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 import {CreateGeneralNews} from '../actions/news';
+
+const TouchableHighlight = styled.TouchableHighlight`
+`;
+const ItemRow = styled.View`
+  flex-flow: row;
+  align-items: center;
+  margin-left: 15px;
+  margin-right: 35px;
+`;
+const TextHead = styled.Text`
+`;
+const TextMain = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+`;
+const TextSub = styled.Text`
+  font-size: 10px;
+  color: #636e72;
+`;
+const Image = styled.Image`
+  border-radius: 10px;
+`
+function Item({ item } ) {
+  let date = new Date(item.datetime * 1000);
+  date = date.toString().slice(0,21);
+  return (
+    <TouchableHighlight
+    activeOpacity={0.6}>
+      <ItemRow>
+        <Col size={90}> 
+          <Row>
+            <TextHead>{item.related}</TextHead>
+          </Row>
+          <Row>
+            <TextMain>{item.headline}</TextMain>
+          </Row>
+          <Row>
+            <TextSub>{date}  </TextSub>
+            <TextSub> {item.source}</TextSub>
+          </Row>
+        </Col>
+        <Col size={10}>
+          <Image source={{uri: item.image}}
+            style={{width: 60, height: 60}} />
+        </Col>
+      </ItemRow>
+    </TouchableHighlight>
+  )
+}
 
 export default function NewsScreen() {
   const dispatch = useDispatch();
@@ -23,7 +74,7 @@ export default function NewsScreen() {
         <View>
           <FlatList
             data={news}
-            renderItem={({ item }) => <Text>{item.headline}</Text>}
+            renderItem={({ item }) => <Item item={item} />}
           />
         </View>
       }
